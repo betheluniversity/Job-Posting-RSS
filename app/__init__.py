de38915d78b967c99123e61a627a9dec053ff9c4
c-schwarze <ces55739@bethel.edu>
2019-03-27 15:19:11 -0500

    add sentry

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
+import logging
+
 import requests.packages.urllib3
 from flask import Flask
+from raven.contrib.flask import Sentry
 
 requests.packages.urllib3.disable_warnings()
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+sentry = Sentry(app, dsn=app.config['SENTRY_URL'], logging=True, level=logging.INFO)
 
 from app.views import JobRSSView
 JobRSSView.register(app, route_base='/')
