f5ee49dd72a16a38c20d2668c273bd7b0641a232
c-schwarze <ces55739@bethel.edu>
2019-03-27 15:28:04 -0500

    move sentry to error level logging

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ requests.packages.urllib3.disable_warnings()
 app = Flask(__name__)
 app.config.from_object('config')
 
-sentry = Sentry(app, dsn=app.config['SENTRY_URL'], logging=True, level=logging.INFO)
+sentry = Sentry(app, dsn=app.config['SENTRY_URL'], logging=True, level=logging.ERROR)
 
 from app.views import JobRSSView
 JobRSSView.register(app, route_base='/')
